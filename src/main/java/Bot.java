bb03af2e52bf696adb1a5340b212bb19d95abc2a
Daas Alexander <alexxdaas@gmail.com>
2018-09-22 12:51:19 +0500

    already bot

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,4 +1,82 @@
-package PACKAGE_NAME;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
-public class Bot {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try{
+            telegramBotsApi.registerBot(new Bot());
+        }
+        catch (TelegramApiRequestException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            setButtons(sendMessage);
+            sendMessage(sendMessage);
+        }
+        catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onUpdateReceived(Update update){
+        Message message = update.getMessage();
+        if (message != null && message.hasText()){
+            switch (message.getText()){
+                case "/help":
+                    sendMsg(message, "Чем могу помочь?");
+                    break;
+                case "/setting":
+                    sendMsg(message, "Что будем настраивать?");
+                    break;
+                default:
+            }
+        }
+    }
+
+    public void setButtons(SendMessage sendMessage){
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboardRowsList = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+
+        keyboardFirstRow.add(new KeyboardButton("/help"));
+        keyboardFirstRow.add(new KeyboardButton("/setting"));
+
+        keyboardRowsList.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRowsList);
+    }
+
+    public String getBotUsername() {
+        return "MyF4kingMusicBot";
+    }
+
+    public String getBotToken() {
+        return "659916332:AAFgUX8BzG47rS1ECuixC985gN-RqqvdoJU";
+    }
 }
