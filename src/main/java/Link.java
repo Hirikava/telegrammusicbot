fec095a2b25eed4a9f0b47bea8ff1db58efbc5a4
artem <artem.gafetinov@gmail.com>
2018-10-02 19:45:49 +0500

    Rename class and methods.

--- src/main/java/Link.java
+++ src/main/java/Link.java
@@ -3,22 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-public class Search {
-    public static String SearchLink(String str){
+public class Link {
+    String name;
+    
+    public Link(String request) {
+        name = SearchLink(request);
+    }
+    
+    private String SearchLink(String str){
         String youtubeLinkForSearching = "https://www.youtube.com/results?search_query=";
         String youtubeSearchingResultLink = youtubeLinkForSearching + str.replaceAll(" ", "+");
         String youtubeLink = "https://www.youtube.com";
-        String result = youtubeLink + GetLink(youtubeSearchingResultLink);
+        String result = youtubeLink + GetVideoId(youtubeSearchingResultLink);
         return result;
     }
 
-    private static String GetLink(String link){
+    private String GetVideoId(String link) {
         try {
             URL site = new URL(link);
             BufferedReader reader = new BufferedReader(new InputStreamReader(site.openStream()));
             String line;
             while ((line = reader.readLine()) != null){
-                System.out.println(line);
                 if (line.startsWith("<li><div class=\"yt-lockup")){
                     String[] wordArr = line.split(" ");
                     for (String word:wordArr) {
@@ -35,5 +40,9 @@ public class Search {
             e.printStackTrace();
         }
         return null;
+    }
+    
+    public String GetName() {
+        return name;
     }
 }
